252f9fde82ba1d8e899086d3208ef4c3834f02ac
Dev.Umar <umarraja230@gmail.com>
2023-02-07 12:09:30 +0500

    AuthProfile Update fn

--- Controller/AuthProfile.js
+++ Controller/AuthProfile.js
@@ -43,4 +43,51 @@ export default {
       );
     }
   },
+  updateProfile: async (req, res, next) => {
+    try {
+      const auth_profile_schema = Joi.object({
+        authprofileID: Joi.string().required(),
+        username: Joi.string().trim(),
+        photoURL: Joi.string().trim(),
+      });
+
+      const validatesResult = await auth_profile_schema.validateAsync(
+        req.body,
+        {
+          errors: true,
+          warnings: true,
+        }
+      );
+      const update_profile = await AuthProfile.update(
+        {
+          username: validatesResult.value.username,
+          photoURL: validatesResult.value.photoURL,
+        },
+        {
+          where: {
+            id: validatesResult.value.authprofileID,
+          },
+        }
+      )
+        .then((resp) => {
+          return res.status(201).send({
+            success: true,
+            message: resp.dataValues,
+          });
+        })
+        .catch((error) => {
+          return next(
+            createHttpError(406, {
+              success: false,
+              message: error.message && error.errors,
+            })
+          );
+        });
+    } catch (error) {
+      // console.log(error.message);
+      return next(
+        createHttpError(406, { success: false, message: error.message })
+      );
+    }
+  },
 };
--- Routes/AuthProfile.js
+++ Routes/AuthProfile.js
@@ -3,5 +3,6 @@ import AuthProfile from "../Controller/AuthProfile.js";
 const router = express.Router();
 
 router.post("/addProfile", AuthProfile.addProfile);
+router.post("/updateProfile", AuthProfile.updateProfile);
 
 export default router;
