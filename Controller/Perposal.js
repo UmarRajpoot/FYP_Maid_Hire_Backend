d67c3bb6f068a4bfb34b975580df28b24722b5e2
Dev.Umar <umarraja230@gmail.com>
2023-02-09 19:52:59 +0500

    Add FCM Notification

--- Controller/Perposal.js
+++ Controller/Perposal.js
@@ -1,6 +1,23 @@
 import createHttpError from "http-errors";
 import Joi from "joi";
 import Perposal from "../Models/Perposal.js";
+import admin from "firebase-admin";
+
+import fs from "fs";
+import path from "path";
+
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+
+// import ServiceAccount from "../maidhiringService.json" assert { type: "json" };
+const loadJSON = (path) =>
+  JSON.parse(fs.readFileSync(new URL(path, import.meta.url)));
+
+const ServiceAccount = loadJSON("../maidhiringService.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(ServiceAccount),
+});
 
 export default {
   createPerposal: async (req, res, next) => {
@@ -39,7 +56,7 @@ export default {
     }
   },
 
-  getperposal: async (req, res, next) => {
+  getMaidperposal: async (req, res, next) => {
     try {
       const register_joi_schema = Joi.object({
         maidId: Joi.string().required().trim(),
@@ -51,9 +68,43 @@ export default {
           warnings: true,
         }
       );
-      const createPerposal = await Perposal.findOne({
+      const getMaidProposal = await Perposal.findOne({
         where: {
           maidId: validatesResult.value.maidId,
+        },
+      })
+        .then((resp) => {
+          return res.status(201).send({
+            success: true,
+            message: resp,
+          });
+        })
+        .catch((error) => {
+          return next(
+            createHttpError(406, { success: false, message: error.message })
+          );
+        });
+    } catch (error) {
+      return next(
+        createHttpError(406, { success: false, message: error.message })
+      );
+    }
+  },
+  getCustomerperposal: async (req, res, next) => {
+    try {
+      const register_joi_schema = Joi.object({
+        customerId: Joi.string().required().trim(),
+      });
+      const validatesResult = await register_joi_schema.validateAsync(
+        req.body,
+        {
+          errors: true,
+          warnings: true,
+        }
+      );
+      const getMaidProposal = await Perposal.findOne({
+        where: {
+          customerId: validatesResult.value.customerId,
         },
       })
         .then((resp) => {
@@ -125,6 +176,38 @@ export default {
             );
           });
       }
+    } catch (error) {
+      return next(
+        createHttpError(406, { success: false, message: error.message })
+      );
+    }
+  },
+
+  appNotification: async (req, res, next) => {
+    try {
+      const registrationToken = req.body.registrationToken;
+      const payload = req.body.message;
+      const message = {
+        notification: {
+          title: "From Nodejs Postman",
+          body: `${payload}`,
+        },
+
+        token: registrationToken,
+      };
+      await admin
+        .messaging()
+        .send(message)
+        .then((response) => {
+          // Response is a message ID string.
+          return res.status(201).send({
+            success: true,
+            message: response,
+          });
+        })
+        .catch((error) => {
+          console.log("Error sending message:", error);
+        });
     } catch (error) {
       return next(
         createHttpError(406, { success: false, message: error.message })
--- Routes/PerposalRoute.js
+++ Routes/PerposalRoute.js
@@ -3,7 +3,10 @@ import Perposal from "../Controller/Perposal.js";
 const router = express.Router();
 
 router.post("/createProposal", Perposal.createPerposal);
-router.post("/getProposal", Perposal.getperposal);
+router.post("/getMaidProposal", Perposal.getMaidperposal);
+router.post("/getCustomerProposal", Perposal.getCustomerperposal);
 router.post("/acceptProposal", Perposal.acceptPerposal);
+
+router.post("/notification", Perposal.appNotification);
 
 export default router;
